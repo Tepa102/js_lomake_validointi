52d5b597ab36ce78368a8a38cb4be771ac36cd9f
Terttu Toivonen <terttu.toivonen@esedulainen.fi>
2023-09-14 18:32:51 +0300

    lisätty ettei tyhjää voi lähettää

--- lomake.js
+++ lomake.js
@@ -59,3 +59,89 @@ function validateForm() {
   }
 }
 
+function validateForm() {
+  const userIDInput = document.getElementById("ID");
+  const salasanaInput = document.getElementById("salasana");
+  const nimiInput = document.getElementById("nimi");
+  const osoiteInput = document.getElementById("osoite");
+  const maaInput = document.getElementById("maa");
+  const postinumeroInput = document.getElementById("postinumero");
+  const emailInput = document.getElementById("email");
+
+  if (userIDInput.value === "" || salasanaInput.value === "" || nimiInput.value === "" || osoiteInput.value === "" || maaInput.value === "" || postinumeroInput.value === "" || emailInput.value === "") {
+    alert("Täytä kaikki kentät ennen lomakkeen lähettämistä.");
+    return false; // Estä lomakkeen lähettäminen
+  }
+
+  return true; // Salli lomakkeen lähettäminen
+}
+function validateForm() {
+  const userIDInput = document.getElementById("ID");
+  const salasanaInput = document.getElementById("salasana");
+  const nimiInput = document.getElementById("nimi");
+  const osoiteInput = document.getElementById("osoite");
+  const sukupuoliInputs = document.querySelectorAll('input[name="sukupuoli"]:checked');
+  const maaInput = document.getElementById("maa");
+  const postinumeroInput = document.getElementById("postinumero");
+  const emailInput = document.getElementById("email");
+  const kieliInputs = document.querySelectorAll('input[name="kieli"]:checked');
+  const genderError = document.getElementById("sukupuoli-error");
+  const userIDError = document.getElementById("IDError");
+  const nimiError = document.getElementById("nimi-error");
+  const postinumeroError = document.getElementById("postinumero-error");
+  const emailError = document.getElementById("email-error");
+
+  if (
+    userIDInput.value === "" || 
+    salasanaInput.value === "" || 
+    nimiInput.value === "" ||
+    sukupuoliInputs.length !== 1 || // Tarkista sukupuolien määrä
+    kieliInputs.length === 0 || // Tarkista kieli-valinnat
+    osoiteInput.value === "" || 
+    maaInput.value === "valitse" || 
+    postinumeroInput.value === "" || 
+    emailInput.value === ""
+  ) {
+    alert("Täytä kaikki kentät ennen lomakkeen lähettämistä.");
+    return false; // Estä lomakkeen lähettäminen
+  }
+
+  // Tarkista ID:n pituus
+  if (userIDInput.value.length < 5) {
+    userIDError.textContent = "Käyttäjä ID:n pituus tulee olla vähintään 5 merkkiä.";
+    return false; // Estä lomakkeen lähettäminen
+  } else {
+    userIDError.textContent = ""; // Tyhjennä virheilmoitus
+  }
+
+  // Tarkista nimen muoto
+  const nimiPattern = /^[A-Za-zÄÖÅäöå\s]+$/;
+  if (!nimiPattern.test(nimiInput.value)) {
+    nimiError.textContent = "Nimi voi sisältää vain kirjaimia ja välilyöntejä.";
+    return false; // Estä lomakkeen lähettäminen
+  } else {
+    nimiError.textContent = ""; // Tyhjennä virheilmoitus
+  }
+
+  // Tarkista postinumeron pituus ja numerot
+  if (!/^\d{5}$/.test(postinumeroInput.value)) {
+    postinumeroError.textContent = "Syötä kelvollinen 5-numeroinen postinumero.";
+    return false; // Estä lomakkeen lähettäminen
+  } else {
+    postinumeroError.textContent = ""; // Tyhjennä virheilmoitus
+  }
+
+  // Tarkista sähköpostin muoto
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (!emailPattern.test(emailInput.value)) {
+    emailError.textContent = 'Syötä kelvollinen sähköpostiosoite.';
+    return false; // Estä lomakkeen lähettäminen
+  } else {
+    emailError.textContent = ""; // Tyhjennä virheilmoitus
+  }
+
+  // Tyhjennä sukupuoli-error-div, jos sukupuoli valittu oikein
+  genderError.textContent = "";
+  
+  return true; // Salli lomakkeen lähettäminen
+}
